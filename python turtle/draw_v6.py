570f5cf3ade926f57a0e2e2385a4e4f53a532d48
Cem <cemcakmak159@gmail.com>
2024-07-16 16:32:37 +0300

    image storage
    added image storage for eps and png files and saves the images to excel file.

--- python turtle/draw_v6.py
+++ python turtle/draw_v6.py
@@ -3,16 +3,31 @@ import random
 import math
 from sympy import symbols, solve
 import openpyxl
+from openpyxl.drawing.image import Image as OpenpyxlImage
 from PIL import Image
+import os
+from datetime import datetime
 
+# Create the 'images' directory if it doesn't exist
+if not os.path.exists('images'):
+    os.makedirs('images')
+
+#Variables
 turtle.title("Vehicle Side Profile")
 screen = turtle.Screen()
 s = turtle.getscreen()
 t = turtle.Turtle()
+canvas = turtle.getcanvas()
 x, y, z = symbols('x y z')
 workbook = openpyxl.load_workbook("Results.xlsx")
 sheet = workbook.active
 next_row = sheet.max_row + 1
+
+
+# Generate a unique filename based on the current date and time
+timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+eps_filename = f'eps_images/drawing_{timestamp}.eps'
+png_filename = f'images/drawing_{timestamp}.png'
 
 
 #Constants
@@ -45,9 +60,9 @@ widthEq = hood*hoodCos+x*frontCos+z+y*backCos-base
 heightEq1 = frontHeight+hood*hoodSin+x*frontSin-totalHeight
 heightEq2 = backHeight+y*backSin-totalHeight
 eq = solve((widthEq, heightEq1, heightEq2),(x, y, z),dict=True)
-windShield = round(eq[0][x],3) #actual value is 66.475
-rearWindow = round(eq[0][y],3) #actual value is 75.563
-roof = round(eq[0][z],3) #actual value is 250.793
+windShield = round(eq[0][x],3) #actual value is 664.75
+rearWindow = round(eq[0][y],3) #actual value is 755.63
+roof = round(eq[0][z],3) #actual value is 2507.93
 
 
 #Print Parameter Results 
@@ -59,24 +74,20 @@ print("Roof Length: ",roof)
 
 
 #Write Outputs to Excel
-sheet.cell(row=next_row, column=1).value = "{:.3f}".format(windShield)
+sheet.cell(row=next_row, column=1).value = "{:.2f}".format(windShield)
 sheet.cell(row=next_row, column=2).value = "{:.2f}".format(frontAngle)
-sheet.cell(row=next_row, column=3).value = "{:.3f}".format(rearWindow)
+sheet.cell(row=next_row, column=3).value = "{:.2f}".format(rearWindow)
 sheet.cell(row=next_row, column=4).value = "{:.2f}".format(backAngle)
-sheet.cell(row=next_row, column=5).value = "{:.3f}".format(roof)
+sheet.cell(row=next_row, column=5).value = "{:.2f}".format(roof)
 workbook.save("Results.xlsx")
-print("Outputs saved to Excel.")
 
 
 # Create turtle screen
 screen.setup(width=800, height=600)
 
 
-# Move the turtle to the starting position
-t.hideturtle()
-
-
 #Draw
+t.hideturtle()
 t.fd(base/10)
 t.lt(90)
 t.fd(backHeight/10)
@@ -93,8 +104,22 @@ t.fd(frontHeight/10)
 
 
 # Save the drawing as an EPS file and convert EPS file to PNG
-canvas = turtle.getcanvas()
-canvas.postscript(file='drawing.eps')
+canvas.postscript(file=eps_filename)
 turtle.done()
-with Image.open('drawing.eps') as img:
-    img.save('drawing.png')
+
+with Image.open(eps_filename) as img:
+    img.save(png_filename)
+
+# Insert the image into the Excel file
+workbook = openpyxl.load_workbook("Results.xlsx")
+sheet = workbook.active
+
+img = OpenpyxlImage(png_filename)
+img.anchor = 'G' + str(next_row)
+sheet.add_image(img)
+
+# Save the workbook
+workbook.save("Results.xlsx")
+workbook.close()
+
+print("Outputs saved to Excel.")
