82618ec740779ace870aa52f489a42e1adf4772d
Cem <cemcakmak159@gmail.com>
2024-07-22 12:15:03 +0300

    parameter test
    a parameter tester code to check if the drawing algorithm or the parameters have an issue
    note: there is an issue

---
+++ python turtle/parameterCheck.py
@@ -0,0 +1,41 @@
+import turtle
+
+backAngle = 78.26
+frontAngle = 58.19
+windShield = 664.75
+rearWindow = 755.63
+roof = 2507.93
+totalWidth = 4166.03
+totalHeight = 1535.63
+base = 4166.03
+roof = 2507.93
+backHeight = 833.01
+hood = 1158.16
+hoodAngle = 4.88
+frontHeight = 862.07
+
+s = turtle.getscreen()
+t = turtle.Turtle()
+screen = turtle.Screen()
+canvas = turtle.getcanvas()
+
+#Draw
+t.hideturtle()
+t.penup()
+t.goto(-totalWidth/10, -totalHeight/10)
+t.pendown()
+t.fd(base/5)
+t.lt(90)
+t.fd(backHeight/5)
+t.lt(90-backAngle)
+t.fd(rearWindow/5)
+t.lt(backAngle)
+t.fd(roof/5)
+t.lt(frontAngle)
+t.fd(windShield/5)
+t.rt(frontAngle-hoodAngle)
+t.fd(hood/5)
+t.lt(90-hoodAngle)
+t.fd(frontHeight/5)
+t.lt(90)
+turtle.done()
--- python turtle/parameterTest.py
+++
@@ -1,8 +0,0 @@
-import random
-from sympy import symbols, solve
-
-x, y = symbols('x y')
-
-a = solve((5*x+12*y-72,18*x-12*y-108),(x,y), dict = True)
-
-print(a[0][x])
