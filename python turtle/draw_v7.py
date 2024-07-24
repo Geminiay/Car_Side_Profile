4d50734fd663c46c2d2fa075fed82348dfb18a34
Cem <cemcakmak159@gmail.com>
2024-07-24 10:44:17 +0300

    v7 update
    update on parameters on v7 and cd order. Image is resized. image destination line removed.

--- python turtle/draw_v7.py
+++ python turtle/draw_v7.py
@@ -9,14 +9,14 @@ import os
 from datetime import datetime
 
 #Constants
-totalWidth = 4166.03
-totalHeight = 1535.63
-base = 4166.03
-roof = 2507.93
-backHeight = 833.01
-hood = 1158.16
-hoodAngle = 4.88
-frontHeight = 862.07
+totalWidth = 4147.65
+totalHeight = 1540.87
+base = 4147.65
+roof = 2493.97
+backHeight = 850.54
+hood = 1150.29
+hoodAngle = 4.68
+frontHeight = 871.27
 hoodCos = math.cos(math.radians(hoodAngle))
 hoodSin = math.sin(math.radians(hoodAngle))
 workbook = openpyxl.Workbook()
@@ -25,7 +25,7 @@ sheet = workbook.active
 def calculateParameters():
     #Parameters
     x, y, z = symbols('x y z')
-    backAngle = round(random.uniform(70,85), 2) #actual value is 78.26
+    backAngle = round(random.uniform(70,85), 2) #actual value is 77.73
     frontAngle = round(random.uniform(50,65), 2) #actual value is 58.19
     frontCos = math.cos(math.radians(frontAngle))
     frontSin = math.sin(math.radians(frontAngle))
@@ -37,11 +37,14 @@ def calculateParameters():
     heightEq1 = frontHeight+hood*hoodSin+x*frontSin-totalHeight
     heightEq2 = backHeight+y*backSin-totalHeight
     eq = solve((widthEq, heightEq1, heightEq2),(x, y, z),dict=True)
-    windShield = round(eq[0][x],2) #actual value is 664.75
-    rearWindow = round(eq[0][y],2) #actual value is 755.63
-    roof = round(eq[0][z],2) #actual value is 2507.93
+    windShield = round(eq[0][x],2) #actual value is 677.49
+    rearWindow = round(eq[0][y],2) #actual value is 706.46
+    roof = round(eq[0][z],2) #actual value is 2493.97
+
+    if isRandom == True: cd = round(random.uniform(0,1), 3)
+    else: cd = None
     
-    return windShield, frontAngle, rearWindow, backAngle, roof
+    return windShield, frontAngle, rearWindow, backAngle, roof, cd
 
 def draw(windShield, frontAngle, rearWindow, backAngle, roof):
 
@@ -50,21 +53,21 @@ def draw(windShield, frontAngle, rearWindow, backAngle, roof):
     #Draw
     t.hideturtle()
     t.penup()
-    t.goto(-totalWidth/20, -totalHeight/20)
+    t.goto(-totalWidth/10, -totalHeight/10)
     t.pendown()
-    t.fd(base/10)
+    t.fd(base/5)
     t.lt(90)
-    t.fd(backHeight/10)
+    t.fd(backHeight/5)
     t.lt(90-backAngle)
-    t.fd(rearWindow/10)
+    t.fd(rearWindow/5)
     t.lt(backAngle)
-    t.fd(roof/10)
+    t.fd(roof/5)
     t.lt(frontAngle)
-    t.fd(windShield/10)
+    t.fd(windShield/5)
     t.rt(frontAngle-hoodAngle)
-    t.fd(hood/10)
+    t.fd(hood/5)
     t.lt(90-hoodAngle)
-    t.fd(frontHeight/10)
+    t.fd(frontHeight/5)
     t.lt(90)
 
 def saveResults(windShield, frontAngle, rearWindow, backAngle, roof, cd):
@@ -75,7 +78,8 @@ def saveResults(windShield, frontAngle, rearWindow, backAngle, roof, cd):
     print("Rear Window Length: ",rearWindow)
     print("Rear Window Angle: ",backAngle)
     print("Roof Length: ",roof)
-    
+    print("Cd: ",cd)
+
     # Next row for the data
     next_row = sheet.max_row + 1
 
@@ -94,9 +98,7 @@ def saveResults(windShield, frontAngle, rearWindow, backAngle, roof, cd):
     sheet.cell(row=next_row, column=3).value = "{:.2f}".format(rearWindow)
     sheet.cell(row=next_row, column=4).value = "{:.2f}".format(backAngle)
     sheet.cell(row=next_row, column=5).value = "{:.2f}".format(roof)
-    sheet.cell(row=next_row, column=6).value = png_filename
-    if isRandom == True:
-        sheet.cell(row=next_row, column=7).value = cd
+    sheet.cell(row=next_row, column=6).value = cd
     
     workbook.save(datasetfile)
     workbook.close()
@@ -119,26 +121,26 @@ os.makedirs(f'dataset-{current_date}')
 os.makedirs(f'dataset-{current_date}/images')
 os.makedirs(f'dataset-{current_date}/eps_images')
 datasetfile = f'dataset-{current_date}/dataset.xlsx'
-sheet.append(["Wind Shield Length", "Wind Shield Angle (50-65)", "Rear Window Length", "Rear Window Angle (70-85)", "Roof Length", "Image Path", "Cd Coefficient" ])
+sheet.append(["Wind Shield Length", "Wind Shield Angle (50-65)", "Rear Window Length", "Rear Window Angle (70-85)", "Roof Length", "Cd" ])
 
 #Create turtle screen
 s = turtle.getscreen()
 t = turtle.Turtle()
 screen = turtle.Screen()
 canvas = turtle.getcanvas()
-screen.setup(width=460, height=200)
+screen.setup(width=920, height=400)
 
 #Draws the original values and saves
-draw(664.75, 58.19, 755.63, 78.26, 2507.93)
-saveResults(664.75, 58.19, 755.63, 78.26, 2507.93, 0.471)
+draw(677.49, 58.19, 706.46, 77.73, 2493.97)
+saveResults(677.49, 58.19, 706.46, 77.73, 2493.97, 0.54)
 t.clear()
 
 #Iteration
 x = 0
 while x < iteration: 
-    windShield, frontAngle, rearWindow, backAngle, roof = calculateParameters()
+    windShield, frontAngle, rearWindow, backAngle, roof, cd = calculateParameters()
     draw(windShield, frontAngle, rearWindow, backAngle, roof)
-    saveResults(windShield, frontAngle, rearWindow, backAngle, roof, round(random.uniform(0,1), 3))
+    saveResults(windShield, frontAngle, rearWindow, backAngle, roof, cd)
     t.clear()
     x += 1
 
